6edbb0263540346c42c68ef0b3ecf2b8ae27edf6
SonicallGameXGit <sonicall.sonicallmusic@gmail.com>
2026-04-02 18:23:20 +0200

    Initial commit

---
+++ src/main/java/me/sgx/clov/Clov.java
@@ -0,0 +1,10 @@
+package me.sgx.clov;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Clov implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/me/sgx/clov/client/ClovClient.java
@@ -0,0 +1,10 @@
+package me.sgx.clov.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ClovClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
